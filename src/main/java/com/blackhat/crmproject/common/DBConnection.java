192f15525ce4b8b49e8b3e4a0e523a80ceca80e0
Ntqkha <ntqkha@gmail.com>
2022-03-05 08:56:08 +0700

    Update DBConnection.java

--- src/main/java/com/blackhat/crmproject/common/DBConnection.java
+++ src/main/java/com/blackhat/crmproject/common/DBConnection.java
@@ -5,16 +5,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+	private static final String url = "jdbc:mysql://localhost:3306/crm";
+	private static final String username = "root";
+	private static final String password = "171197";
+	
 	public static Connection getConnection() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/crm", "root", "A-z!23456");
+			Connection conn = DriverManager.getConnection(url, username, password);
 			return conn;
 		}catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Không tìm thấy driver cơ sở dữ liệu.");
 			e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Không thể kết nối đến cơ sở dữ liệu.");
 			e.printStackTrace();
 		}
 		return null;
